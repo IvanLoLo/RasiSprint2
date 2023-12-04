a5f20191bf6dffa0bdc3926a5100e863e68fe3af
Ivan Yared Lombana Lozano <ilombana@unal.edu.co>
2023-12-04 12:38:31 -0500

    Indent

--- citas/views.py
+++ citas/views.py
@@ -12,7 +12,7 @@ from rasi.auth0backend import getRole
 def citas_view(request):
     role = getRole(request)
     if request.method == 'GET':
-        if role not in ['Admin', 'Medico']: return HttpResponse("Unauthorized User")
+        if role not in ['Admin', 'Medico']: return HttpResponse("Unauthorized User", status=401)
         id = request.GET.get('id', None)
         if id:
             cita = vl.get_cita(id)
@@ -33,7 +33,7 @@ def citas_view(request):
 def cita_view(request, pk):
     role = getRole(request)
     if request.method == 'GET':
-        if role not in ['Admin', 'Medico']: return HttpResponse("Unauthorized User")
+        if role not in ['Admin', 'Medico']: return HttpResponse("Unauthorized User", status=401)
         citas = vl.get_cita(pk)
         citas_dto = serializers.serialize('json', [citas,])
         return HttpResponse(citas_dto, 'application/json')
@@ -45,7 +45,7 @@ def cita_view(request, pk):
         
 @csrf_exempt
 def paciente_view(request, pk=None):
-    if request.method == 'GET': paciente_get_view(request, pk)
+    if request.method == 'GET': return paciente_get_view(request, pk)
     if request.method == 'POST':
         paciente_dto = vl.create_paciente(json.loads(request.body))
         paciente = serializers.serialize('json', [paciente_dto,])
@@ -54,7 +54,7 @@ def paciente_view(request, pk=None):
 @login_required
 def paciente_get_view(request, pk=None):
     role = getRole(request)
-    if role not in ['Admin', 'Medico']: return HttpResponse("Unauthorized User")
+    if role not in ['Admin', 'Medico']: return HttpResponse("Unauthorized User", status=401)
     if pk:
         paciente = vl.get_paciente(pk)
         paciente_dto = serializers.serialize('json', [paciente,])
@@ -69,7 +69,7 @@ def paciente_get_view(request, pk=None):
 def medico_view(request, pk=None):
     role = getRole(request)
     if request.method == 'GET':
-        if role not in ['Admin', 'Medico']: return HttpResponse("Unauthorized User")
+        if role not in ['Admin', 'Medico']: return HttpResponse("Unauthorized User", status=401)
         if pk:
             medico = vl.get_doctor(pk)
             medico_dto = serializers.serialize('json', [medico,])
@@ -80,7 +80,7 @@ def medico_view(request, pk=None):
             return HttpResponse(medicos_dto, 'application/json')
         
     if request.method == 'POST':
-        if role != 'Admin': return HttpResponse("Unauthorized User")
+        if role != 'Admin': return HttpResponse("Unauthorized User", status=401)
         medico_dto = vl.create_doctor(json.loads(request.body))
         medico = serializers.serialize('json', [medico_dto,])
         return HttpResponse(medico, 'application/json')
@@ -88,7 +88,7 @@ def medico_view(request, pk=None):
 @login_required    
 def cita_by_fecha_view(request, fecha):
     role = getRole(request)
-    if role != 'Admin': return HttpResponse("Unauthorized User")
+    if role != 'Admin': return HttpResponse("Unauthorized User", status=401)
     if request.method == 'GET':
         citas = vl.get_citas_by_fecha(fecha)
         citas_dto = json.dumps({"total_citas": citas})
@@ -97,7 +97,7 @@ def cita_by_fecha_view(request, fecha):
 @login_required
 def cita_by_mes_view(request, mes):
     role = getRole(request)
-    if role != 'Admin': return HttpResponse("Unauthorized User")
+    if role != 'Admin': return HttpResponse("Unauthorized User", status=401)
     if request.method == 'GET':
         citas = vl.get_citas_by_mes(mes)
         citas_dto = json.dumps({"total_citas": citas})
@@ -106,7 +106,7 @@ def cita_by_mes_view(request, mes):
 @login_required    
 def cita_by_especialidad_view(request, especialidad):
     role = getRole(request)
-    if role not in ['Admin', 'Medico']: return HttpResponse("Unauthorized User")
+    if role not in ['Admin', 'Medico']: return HttpResponse("Unauthorized User", status=401)
     if request.method == 'GET':
         citas = vl.get_citas_by_especialidad(especialidad)
         citas_dto = serializers.serialize('json', citas)
